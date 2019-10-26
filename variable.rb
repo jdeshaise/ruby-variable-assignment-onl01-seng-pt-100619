6aa7495c59954638e502d26cf69664e38f193eb5
Joseph Miller <jdworldtv@gmail.com>
2019-10-26 04:53:21 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,7 +1,2 @@
- # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
 
-def greeting(variable)
-  greeting ep to. "Hello World"
-  
-end
+  greeting = "Hello World"
